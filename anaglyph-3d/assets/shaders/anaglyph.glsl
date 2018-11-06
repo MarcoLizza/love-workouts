9f9a13ba42e53b6fe49b9ff924f4640c8cc31c6c
Marco Lizza <marco.lizza@datalogic.com>
2018-11-06 17:43:40 +0100

    Implementing anaglyph shader.

--- anaglyph-3d/assets/shaders/anaglyph.glsl
+++ anaglyph-3d/assets/shaders/anaglyph.glsl
@@ -1,18 +1,98 @@
+const float DISTANCE_MULTIPLIER = 4.0;
+
+// https://pdfs.semanticscholar.org/1db2/b741bedb782b4b4b67ec8a648b7fecf58ef8.pdf
 uniform Image _depth_map;
-uniform float _time;
+uniform float _offset = 0.0125;
+uniform int _mode = 0;
+uniform vec2 _vanishing_point = vec2(0.5, 0.5);
 
-float offset(vec4 depth) {
+// http://3dtv.at/Knowhow/AnaglyphComparison_en.aspx
+vec3 grey_anaglyph(vec3 pixel_left, vec3 pixel_right) {
+    mat3 matrix_left = mat3( // Column first.
+        0.299, 0.000, 0.000,
+        0.587, 0.000, 0.000,
+        0.114, 0.000, 0.000
+    );
+    mat3 matrix_right = mat3( // Column first.
+        0.000, 0.299, 0.299,
+        0.000, 0.587, 0.587,
+        0.000, 0.114, 0.114
+    );
+    return matrix_left * pixel_left + matrix_right * pixel_right;
+}
 
-    return depth.r * 
+vec3 color_anaglyph(vec3 pixel_left, vec3 pixel_right) {
+    mat3 matrix_left = mat3( // Column first.
+        1.000, 0.000, 0.000,
+        0.000, 0.000, 0.000,
+        0.000, 0.000, 0.000
+    );
+    mat3 matrix_right = mat3( // Column first.
+        0.000, 0.000, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000
+    );
+    return matrix_left * pixel_left + matrix_right * pixel_right;
+}
 
+vec3 halfcolor_anaglyph(vec3 pixel_left, vec3 pixel_right) {
+    mat3 matrix_left = mat3( // Column first.
+        0.299, 0.000, 0.000,
+        0.587, 0.000, 0.000,
+        0.114, 0.000, 0.000
+    );
+    mat3 matrix_right = mat3( // Column first.
+        0.000, 0.000, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000
+    );
+    return matrix_left * pixel_left + matrix_right * pixel_right;
+}
+
+vec3 optimized_anaglyph(vec3 pixel_left, vec3 pixel_right) {
+    mat3 matrix_left = mat3( // Column first.
+        0.000, 0.000, 0.000,
+        0.700, 0.000, 0.000,
+        0.300, 0.000, 0.000
+    );
+    mat3 matrix_right = mat3( // Column first.
+        0.000, 0.000, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000
+    );
+    return pow(matrix_left * pixel_left + matrix_right * pixel_right, vec3(1.0 / 1.5, 1.0, 1.0)); // Gamma encoding, to correct RED component.
 }
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-//    vec2 uv = screen_coords / vec2(love_ScreenSize);
-
     vec4 depth = texture2D(_depth_map, texture_coords);
-    float time = _time;
 
-    vec4 pixel = texture2D(texture, texture_coords);
-    return pixel;
+    // Compute the x offset, basing of the depth map pixel (which is a black-and-white
+    // image with back as "no offset" and white as "max offset").
+    float factor = depth.r;
+    float offset = factor * _offset;
+    // Alse the distance from the vanishing point should be used control the offset.
+    vec2 xy = screen_coords / vec2(love_ScreenSize);
+    offset *= distance(xy, _vanishing_point) * DISTANCE_MULTIPLIER;
+
+    // Negative offset for RED channel.
+    vec2 uv_left = vec2(texture_coords.x - offset, texture_coords.y);
+    vec2 uv_right = vec2(texture_coords.x + offset, texture_coords.y);
+
+    vec3 pixel_left = texture2D(texture, uv_left).rgb;
+    vec3 pixel_right = texture2D(texture, uv_right).rgb;
+
+    vec3 pixel;
+    if (_mode == 0) {
+        pixel = grey_anaglyph(pixel_left, pixel_right);
+    } else
+    if (_mode == 1) {
+        pixel = color_anaglyph(pixel_left, pixel_right);
+    } else
+    if (_mode == 2) {
+        pixel = halfcolor_anaglyph(pixel_left, pixel_right);
+    } else
+    if (_mode == 3) {
+        pixel = optimized_anaglyph(pixel_left, pixel_right);
+    }
+    return vec4(pixel, 1.0);
 }
--- anaglyph-3d/main.lua
+++ anaglyph-3d/main.lua
@@ -20,12 +20,19 @@ freely, subject to the following restrictions:
 
 ]] --
 
+local MODES = { 'grey', 'color', 'halfcolor', 'optimized' }
+
 local unpack = unpack or table.unpack
 
 local _time = 0
 local _debug = false
+
 local _shader = nil
+local _image = nil
 local _depth_map = nil
+local _offset = 0.0125
+local _mode = 0
+local _vanishing_point = { 0.5, 0.5 }
 
 function love.load(args)
   love.graphics.setDefaultFilter('nearest', 'nearest', 1)
@@ -43,35 +50,43 @@ function love.load(args)
   end
 
   _depth_map = love.graphics.newImage('data/depth-map.png')
+  _image = love.graphics.newImage('data/image.png')
 
   _shader = love.graphics.newShader('assets/shaders/anaglyph.glsl')
 end
 
 function love.update(dt)
   _time = _time + dt
-
-  _shader:send('_time', _time)
 end
 
 function love.draw()
   love.graphics.push('all')
     _shader:send('_depth_map', _depth_map)
+    _shader:send('_offset', _offset)
+    _shader:send('_mode', _mode)
+    _shader:send('_vanishing_point', _vanishing_point)
     love.graphics.setShader(_shader)
     love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
 --    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-    love.graphics.draw(_depth_map)
+    love.graphics.draw(_image)
   love.graphics.pop()
 
-  love.graphics.setColor(1.0, 1.0, 1.0, 0.5)
+  love.graphics.setColor(0.0, 0.0, 0.0, 0.5)
   love.graphics.print(love.timer.getFPS() .. ' FPS', 0, 0)
+  love.graphics.print(string.format('mode %s, offset %.4f, vanishing-point <%.2f, %.2f>', MODES[_mode + 1], _offset, _vanishing_point[1], _vanishing_point[2]), 0, 16)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
+  _vanishing_point = { x / love.graphics.getWidth(), y / love.graphics.getHeight() }
 end
 
 function love.keypressed(key, scancode, isrepeat)
   if key == 'f1' then
+    _mode = (_mode + 1) % #MODES
   elseif key == 'f2' then
+    _offset = _offset - 0.0005
+  elseif key == 'f3' then
+    _offset = _offset + 0.0005
   elseif key == 'f12' then
     _debug = not _debug
   end
