20e875811da3cd22a2240161815744495cdc2dcd
Marco Lizza <marco.lizza@datalogic.com>
2018-11-15 17:34:45 +0100

    Passing the texture size as uniform variable to the shader.

--- anaglyph-3d/assets/shaders/parallax.glsl
+++ anaglyph-3d/assets/shaders/parallax.glsl
@@ -1,10 +1,10 @@
 uniform int _speed;
 uniform float _offset;
-//uniform float _time;
+uniform vec2 _texture_size;
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
     vec2 offset = vec2(_speed * _offset, 0.0);
-    vec2 uv = texture_coords + (offset / vec2(love_ScreenSize));
+    vec2 uv = texture_coords + (offset / _texture_size);
     return texture2D(texture, uv);
 }
